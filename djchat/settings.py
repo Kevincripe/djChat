556dd1f7dca6c4a014ec6522074637b48af496bf
Kevin Cripe <kevincripe6@gmail.com>
2025-05-13 11:24:45 -0500

    account-server models

--- djchat/settings.py
+++ djchat/settings.py
@@ -28,6 +28,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account',
+    'server',
 ]
 
 MIDDLEWARE = [
@@ -111,3 +113,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'account.Account'
