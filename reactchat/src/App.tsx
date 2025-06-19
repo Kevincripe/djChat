bfe35b9ba9b670cdffa99af68b6d7bff3d0b1d6d
Kevin Cripe <kevincripe6@gmail.com>
2025-06-19 18:56:00 -0500

    home page

--- reactchat/src/App.tsx
+++ reactchat/src/App.tsx
@@ -1,12 +1,16 @@
+import Home from "./pages/Home";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route>
+            <Route path="/" element={<Home />}/>
+        </Route>
+    )
+);
 
-function App() {
-
-  return (
-    <>
-      
-    </>
-  )
-}
+const App = () => {
+    return <RouterProvider router={router} />;
+};
 
 export default App
