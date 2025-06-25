f50f2bd2602e30509255779a878a12779d4f6699
Kevin Cripe <kevincripe6@gmail.com>
2025-06-25 12:54:22 -0500

    corsheaders

--- server/models.py
+++ server/models.py
@@ -1,33 +1,38 @@
-from django.db import models
 from django.conf import settings
-from django.shortcuts import get_object_or_404
+from django.db import models
 from django.dispatch import receiver
+from django.shortcuts import get_object_or_404
 
 from .validators import validate_icon_image_size, validate_image_file_exstension
 
+
 def server_icon_upload_path(instance, filename):
-    return f"server/{instance.id}/server_icon/{filename}"
+    return f"server/{instance.id}/server_icons/{filename}"
+
 
 def server_banner_upload_path(instance, filename):
     return f"server/{instance.id}/server_banner/{filename}"
 
+
 def category_icon_upload_path(instance, filename):
     return f"category/{instance.id}/category_icon/{filename}"
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     icon = models.FileField(
-        upload_to=category_icon_upload_path, 
-        null=True, 
-        blank=True)
-    
+        upload_to=category_icon_upload_path,
+        null=True,
+        blank=True,
+    )
+
     def save(self, *args, **kwargs):
         if self.id:
             existing = get_object_or_404(Category, id=self.id)
             if existing.icon != self.icon:
                 existing.icon.delete(save=False)
-                super(Category, self).save(*args, **kwargs)
+        super(Category, self).save(*args, **kwargs)
 
     @receiver(models.signals.pre_delete, sender="server.Category")
     def category_delete_files(sender, instance, **kwargs):
@@ -45,11 +50,12 @@ class Server(models.Model):
     name = models.CharField(max_length=100)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="server_owner")
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="server_category")
-    description = models.CharField(max_length=250,blank=True, null=True)
+    description = models.CharField(max_length=250, blank=True, null=True)
     member = models.ManyToManyField(settings.AUTH_USER_MODEL)
 
     def __str__(self):
         return f"{self.name}-{self.id}"
+
 
 class Channel(models.Model):
     name = models.CharField(max_length=100)
@@ -74,9 +80,9 @@ class Channel(models.Model):
             existing = get_object_or_404(Category, id=self.id)
             if existing.icon != self.icon:
                 existing.icon.delete(save=False)
-                if existing.banner != self.banner:
-                    existing.banner.delete(save=False)
-                super(Category, self).save(*args, **kwargs)
+            if existing.banner != self.banner:
+                existing.banner.delete(save=False)
+        super(Category, self).save(*args, **kwargs)
 
     @receiver(models.signals.pre_delete, sender="server.Server")
     def category_delete_files(sender, instance, **kwargs):
